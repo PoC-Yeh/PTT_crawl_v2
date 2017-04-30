74f7efee5eeba081b8a34bbf37370e22dddb625f
PoC-Yeh <allisonxp7@gmail.com>
2017-04-30 12:04:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 import progressbar
 from datetime import datetime
-from common_and_over18 import get_page_url_over18
-from common_and_over18 import get_page_url_common
-from get_text import get_meta_text
+from get_url import get_page_url_over18
+from get_url import get_page_url_common
+from get_text import get_meta_text_over18
+from get_text import get_meta_text_common
 
 #pages for adults only
 over18_url_list, over18_short_url_list = get_page_url_over18("Gossiping", 23175) #board name, end-page
 
 over18_text_list = []
 for i in range(0, len(over18_url_list)):
-    text_data = get_meta_text(i)
+    text_data = get_meta_text_over18(i)
     over18_text_list.append(text_data)
     
 for text_a in over18_text_list:
@@ -22,7 +23,7 @@ for text_a in over18_text_list:
 common_url_list, common_short_url_list = get_page_url_common("Makeup", 2566)
 common_text_list = []
 for i in range(0, len(common_url_list)):
-    text_data = get_meta_text(i)
+    text_data = get_meta_text_common(i)
     common_text_list.append(text_data)
     
 for text_c in common_text_list:
